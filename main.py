94799cc0607d555ecabaeb521822f1fcc6a82cfd
Petz <peter.stroissnig@student.tugraz.at>
2021-11-09 21:15:32 +0100

    total workover of gui

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter.font import Font
+from enum import Enum
+from tkinter.messagebox import showinfo
+
+from PIL import ImageTk, Image
 
 combatant_list = []
 player_list = []
 turn_counter = 1
 
+class BGColor(Enum):
+    BG_Image = 0
+    Dark_Mode = 1
+    Bright_Mode = 2
+    Modern = 3
 
 class Combatant:
     name = ""
@@ -22,7 +32,7 @@ class Combatant:
 
 def next_turn():
     global turn_counter
-    print(f"Runde: {turn_counter}")
+    #print(f"Runde: {turn_counter}")
     turn_counter += 1
     player = combatant_list[0]
     # player.turn_player = True
@@ -31,6 +41,8 @@ def next_turn():
     combatant_list.pop(0)
     combatant_list.append(player)
     update_listbox()
+    #print(str(tree.index(0)[1]))
+    #tree.insert("", END, values=(tree[0][0], tree[0][1]))
 
 
 def create_team():
@@ -49,12 +61,30 @@ def add_player():
     combatant_list.append(player)
     update_listbox()
 
+    # add to tree
+    tree.insert("", END, values=(player.init_value, player.name))
+
 
 def sort_players():
     # sort list
     combatant_list.sort()
     combatant_list.reverse()
     update_listbox()
+
+    # sort treeview
+    treeview_sort_column(tree, tree.column("init"), False)
+
+def treeview_sort_column(tv, col, reverse):
+    l = [(tv.set(k, col), k) for k in tv.get_children('')]
+    l.sort(reverse=reverse)
+
+    # rearrange items in sorted positions
+    for index, (val, k) in enumerate(l):
+        tv.move(k, '', index)
+
+    # reverse sort next time
+    tv.heading(col, command=lambda: \
+               treeview_sort_column(tv, col, not reverse))
 
 
 def edit():
@@ -68,6 +98,11 @@ def edit():
 
 
 def remove_player():
+    selected = tree.focus()
+    print(tree.item(selected, 'values')[1])
+    tree.delete(tree.focus())
+
+    print(listbox.curselection()[0])
     combatant_list.pop(listbox.curselection()[0])
     update_listbox()
 
@@ -88,21 +123,35 @@ def print_combatant_list():
 
 
 if __name__ == "__main__":
-    # fill up list with hardcoded team members
+    # fill up list with names from input file
     create_team()
 
     # gui
-    dark_mode = True
+    change_background = BGColor.Modern
+
     window = Tk()
+    path = "res/wood.jpg"
+    filename = ImageTk.PhotoImage(Image.open(path))
+    if change_background == BGColor.BG_Image:
+        background_label = Label(window, image=filename)
+        background_label.place(x=0, y=0)
+    elif change_background == BGColor.Dark_Mode:
+        window.configure(background="#3B4A4C")
+    elif change_background == BGColor.Bright_Mode:
+        window.configure(background="#D6E1F4")
+    elif change_background == BGColor.Modern:
+        window.configure(background="#3679AD")
+
     window.title("Initiative Tracker")
-    window.geometry("400x340")
-    window.iconbitmap("d20.ico")
+    window.geometry("600x440")
+    window.iconbitmap("res/d20.ico")
     font_b = Font(size=12)
-    font_bb = Font(size=14)
-    listbox = Listbox(window, height=10, font=font_bb, exportselection=False)
+    font_lb = Font(size=14)
+
+
+    listbox = Listbox(window, bg="#9B95A1", height=10, font=font_lb, exportselection=False)
     for element in player_list:
         listbox.insert(END, str(element.init_value) + " " + element.name)
-
 
     scrollbar = Scrollbar(window, orient=VERTICAL)
     listbox.config(yscrollcommand=scrollbar.set)
@@ -114,7 +163,33 @@ if __name__ == "__main__":
 
     scrollbar.grid(row=0, column=2, rowspan=4, sticky=N + S, padx=(0, 10), pady=10)
 
-    next_button = Button(text="NEXT", command=next_turn, height=3, width=8, font=font_bb)
+
+
+    # tree
+    columns = ('init', 'char_name')
+
+    tree = ttk.Treeview(window, columns=columns, show='headings')
+    tree.column("init", minwidth=20, width=50, stretch=NO, anchor=CENTER)
+    tree.column("char_name", minwidth=50, width=100, stretch=NO, anchor=CENTER)
+    # define headings
+    tree.heading('init', text='Value')
+    tree.heading('char_name', text='Character')
+
+    # add data to the treeview
+    for n in player_list:
+        tree.insert("", END, values=(str(n.init_value), n.name))
+
+    tree.grid(row=0, column=8, sticky='nsew', pady=10)
+
+    # add a scrollbar
+    scrollbar = ttk.Scrollbar(window, orient=VERTICAL, command=tree.yview)
+    tree.configure(yscroll=scrollbar.set)
+    scrollbar.grid(row=0, column=9, sticky='ns', pady=10)
+
+
+
+    # next button needed to skip the selection to the next player
+    next_button = Button(text="NEXT", bg="#9B95A1", command=next_turn, height=3, width=8, font=font_lb)
     next_button.grid(row=1, column=3, sticky=EW, padx=10, pady=10)
 
     name_label = Label(window, text="Name:", height=1, width=5)
@@ -129,20 +204,17 @@ if __name__ == "__main__":
     init_entry = Entry(window, width=8)
     init_entry.grid(row=5, column=0, padx=(35, 0), pady=10)
 
-    add_button = Button(text="+", command=add_player, height=1, width=4)
-    add_button.grid(row=4, column=1,sticky=W, padx=(0, 0))
+    add_button = Button(text="+", bg="#9B95A1", command=add_player, height=1, width=4)
+    add_button.grid(row=4, column=1, sticky=W, padx=(0, 0))
 
-    remove_button = Button(text="-", command=remove_player, height=1, width=4)
+    remove_button = Button(text="-", bg="#9B95A1", command=remove_player, height=1, width=4)
     remove_button.grid(row=5, column=1, sticky=W, padx=(0, 0))
 
-    edit_button = Button(text="EDIT", command=edit, height=1, width=4)
+    edit_button = Button(text="EDIT", bg="#9B95A1", command=edit, height=1, width=4)
     edit_button.grid(row=4, column=1, sticky=E, padx=(0, 0))
 
-    sort_button = Button(text="SORT", command=sort_players, height=1, width=4)
+    sort_button = Button(text="SORT", bg="#9B95A1", command=sort_players, height=1, width=4)
     sort_button.grid(row=5, column=1, sticky=E, padx=(0, 0))
-
-    if dark_mode:
-        window.configure(background="lightgrey")
 
     update_listbox()
 
